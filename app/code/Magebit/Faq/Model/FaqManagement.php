c253b40f6a311dcc0dceeed2084ef2ee86b71b9c
Eriks Marsans <eriks.marsans@magebit.com>
2023-08-10 17:30:26 +0300

    remove Repository from FaqManagement

--- app/code/Magebit/Faq/Model/FaqManagement.php
+++ app/code/Magebit/Faq/Model/FaqManagement.php
@@ -6,26 +6,16 @@ namespace Magebit\Faq\Model;
 
 use Magebit\Faq\Api\Data\FaqInterface;
 use Magebit\Faq\Api\FaqManagementInterface;
-use Magebit\Faq\Api\FaqRepositoryInterface;
 
 class FaqManagement implements FaqManagementInterface
 {
-    public function __construct(
-        private FaqRepositoryInterface $faqRepository
-    ) {
-    }
-
     public function enableFaq(FaqInterface $faq): void
     {
         $faq->setStatus($faq::STATUS_ENABLED);
-
-        $this->faqRepository->save($faq);
     }
 
     public function disableFaq(FaqInterface $faq): void
     {
         $faq->setStatus($faq::STATUS_DISABLED);
-
-        $this->faqRepository->save($faq);
     }
 }
